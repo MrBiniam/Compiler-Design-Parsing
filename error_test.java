a2bc9656105e7ace3bd9a1dd0d3e60013ba73e4a
MrBiniam <biniams.shimeles@gmail.com>
2025-04-16 13:49:06 +0300

    Add error_test.java to demonstrate syntax error detection

---
+++ error_test.java
@@ -0,0 +1,10 @@
+public class ErrorTest {
+    public static void main(String[] args) {
+        // Missing semicolon - intentional syntax error
+        int x = 10
+        
+        if (x > 5) {
+            System.out.println("Greater than 5");
+        }
+    }
+}
